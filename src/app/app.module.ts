f8e2c43002636e7c052be9a3c7452ef42279374a
Devin <devdhera@gmail.com>
2019-09-17 11:23:27 +0530

    Base setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
