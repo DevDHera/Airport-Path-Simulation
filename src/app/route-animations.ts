25ae5746a727d7746d9fa2ac3e4ebdf56fa7ba45
Devin <devdhera@gmail.com>
2019-09-18 21:06:27 +0530

    Fixed fader errors

--- src/app/route-animations.ts
+++ src/app/route-animations.ts
@@ -23,10 +23,10 @@ export const fader =
                     opacity: 0,
                     transform: 'scale(0) translateY(100%)',
                 }),
-            ]),
+            ], { optional: true }),
             query(':enter', [
                 animate('600ms ease', style({ opacity: 1, transform: 'scale(1) translateY(0)' })),
-            ])
+            ], { optional: true })
         ]),
     ]);
 
