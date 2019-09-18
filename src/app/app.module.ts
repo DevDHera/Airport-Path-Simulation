e0bf1faa4907d212be740c2016f6d070b514f84e
Devin <devdhera@gmail.com>
2019-09-18 14:47:24 +0530

    Reconfigured routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MapComponent } from './map/map.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
+import { MapComponent } from './components/map/map.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    LandingComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/map/map.component.ts
@@ -0,0 +1,119 @@
+import { Component, OnInit } from '@angular/core';
+import Map from 'ol/Map';
+import Tile from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
+import Feature from 'ol/Feature';
+import sVector from 'ol/source/Vector';
+import lVector from 'ol/layer/Vector';
+import Point from 'ol/geom/Point';
+import { fromLonLat } from 'ol/proj.js';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import TileJSON from 'ol/source/TileJSON';
+import VectorSource from 'ol/source/Vector';
+import { Icon, Style } from 'ol/style';
+import { environment } from '../../../environments/environment';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+
+  map;
+  chicago;
+  vectorSource;
+  vectorLayer;
+  rasterLayer;
+  london: any;
+  madrid: any;
+  mapboxPK = environment.mapboxPK;
+
+  constructor() { }
+
+  ngOnInit() {
+    // this.initializeMap();
+    this.initializeMapWithMarkers();
+  }
+
+  initializeMap() {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new Tile({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [37.41, 8.82],
+        zoom: 4
+      })
+    });
+  }
+
+  initializeMapWithMarkers() {
+    this.chicago = new Feature({
+      geometry: new Point(fromLonLat([-87.623177, 41.881832]))
+    });
+
+    this.chicago.setStyle(new Style({
+      image: new Icon(({
+        color: '#8959A8',
+        crossOrigin: 'anonymous',
+        src: 'assets/vectorpoint.svg',
+        imgSize: [20, 20]
+      }))
+    }));
+
+    this.london = new Feature({
+      geometry: new Point(fromLonLat([-0.12755, 51.507222]))
+    });
+
+    this.madrid = new Feature({
+      geometry: new Point(fromLonLat([-3.683333, 40.4]))
+    });
+
+    this.london.setStyle(new Style({
+      image: new Icon(({
+        color: '#4271AE',
+        crossOrigin: 'anonymous',
+        src: 'assets/vectorpoint.svg',
+        imgSize: [20, 20]
+      }))
+    }));
+
+    this.madrid.setStyle(new Style({
+      image: new Icon(({
+        color: [113, 140, 0],
+        crossOrigin: 'anonymous',
+        src: 'assets/dot.png',
+        imgSize: [20, 20]
+      }))
+    }));
+
+    this.vectorSource = new VectorSource({
+      features: [this.chicago, this.madrid, this.london]
+    });
+
+    this.vectorLayer = new VectorLayer({
+      source: this.vectorSource
+    });
+
+    this.rasterLayer = new TileLayer({
+      source: new TileJSON({
+        url: 'https://api.tiles.mapbox.com/v4/mapbox.geography-class.json?secure&access_token=' + this.mapboxPK,
+        crossOrigin: ''
+      })
+    });
+
+    this.map = new Map({
+      target: 'map',
+      layers: [this.rasterLayer, this.vectorLayer],
+      view: new View({
+        center: fromLonLat([2.896372, 44.60240]),
+        zoom: 3
+      })
+    });
+  }
+}
