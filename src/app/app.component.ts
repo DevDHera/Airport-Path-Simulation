57f321ff110301b8ec5c8ba39b2febbe65616772
Devin <devdhera@gmail.com>
2019-09-18 20:36:44 +0530

    :zap: Configured route animations :zap:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, Router, Event, NavigationStart } from '@angular/router';
+import { slider, transformer, fader, stepper } from './route-animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    fader,
+    // slider,
+    // transformer,
+    // stepper
+  ]
 })
 export class AppComponent {
   title = 'airport-simulation';
+  footerVisibility = false;
+
+  constructor(private router: Router) {
+
+    router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        this.footerVisibility = false;
+      } else {
+        setTimeout(() => { this.footerVisibility = true }, 1000);
+      }
+    });
+
+  }
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
 }
