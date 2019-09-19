ae769473d2dbb3f24ca96f4efbb429d77bb5c613
Devin <devdhera@gmail.com>
2019-09-19 17:51:07 +0530

    Integrated map with airports

--- src/app/pages/airports/airports.component.ts
+++ src/app/pages/airports/airports.component.ts
@@ -1,20 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { AirportService } from '../../services/airport.service';
 import { Airport } from '../../models/Airport';
+import { Marker } from '../../models/Marker';
 
 @Component({
   selector: 'app-airports',
   templateUrl: './airports.component.html',
   styleUrls: ['./airports.component.scss']
 })
-export class AirportsComponent implements OnInit {
+export class AirportsComponent implements OnInit, OnDestroy {
+  dtOptions: DataTables.Settings = {};
+  dtTrigger = new Subject();
   airports: Airport[];
+  markers: Marker[];
 
   constructor(private airportService: AirportService) { }
 
   ngOnInit() {
-    this.airportService.getAirports().subscribe(airports => this.airports = airports);
+    this.dtOptions = {
+      pagingType: 'simple_numbers',
+      pageLength: 2
+    };
+
+    this.airportService.getAirports().subscribe(airports => {
+      this.airports = airports;
+      this.markers = this.airports.map(({ id, name, iata, position, ...rest }) => Object.assign({
+        lat: position.lat,
+        lng: position.lng,
+        label: name
+      }));
+      this.dtTrigger.next();
+    });
+  }
+
+  ngOnDestroy() {
+    this.dtTrigger.unsubscribe();
   }
 
 }
