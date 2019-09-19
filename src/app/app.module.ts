b6a22bd483f4b6b37c6fe3cd28c6054176e2e098
Devin <devdhera@gmail.com>
2019-09-19 14:50:01 +0530

    :fire: Init firebase :fire:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +13,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AirportsComponent } from './pages/airports/airports.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +23,15 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
     FooterComponent,
     LandingComponent,
     AboutUsComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    AirportsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase, 'air-sim'),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-  mapboxPK: 'pk.eyJ1IjoiZGV2ZGhlcmEiLCJhIjoiY2swbmc2bmxiMWQ4ejNibnM0czYzbWsxcSJ9.Zw0nssbFBVEoshp2uB6AHw'
+  mapboxPK: 'pk.eyJ1IjoiZGV2ZGhlcmEiLCJhIjoiY2swbmc2bmxiMWQ4ejNibnM0czYzbWsxcSJ9.Zw0nssbFBVEoshp2uB6AHw',
+  firebase: {
+    apiKey: "AIzaSyBzB7Q5oFuOfkLqAHVbpWXzwq8A3sIdTGc",
+    authDomain: "air-sim-90a3f.firebaseapp.com",
+    databaseURL: "https://air-sim-90a3f.firebaseio.com",
+    projectId: "air-sim-90a3f",
+    storageBucket: "air-sim-90a3f.appspot.com",
+    messagingSenderId: "979128466658",
+    appId: "1:979128466658:web:56c6622d5f9a0abce4b2bb"
+  }
 };
 
 /*
